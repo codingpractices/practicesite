0222ad2dfddd170878d907539f97eafd9cf0bcf6
Josh-D-L <zepieguy@gmail.com>
2014-05-16 17:06:31 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,3 +1,27 @@
-var continue=confirm("do you really want to play this game?");
-console.log(continue)
-if(continue=)
+confirm("do you really want to play this game?"){
+  var slaying = true;
+  var youHit = Math.floor(Math.random() * 2);
+  var damageThisRound = Math.floor(Math.random()*5 + 1);
+  var totaldamage = 0;
+  while(slaying) {
+        if(youHit = true)
+       {
+          console.log("You hit the dragon!");
+          totalDamage += damageThisRound;
+          if(totalDamage >= 4) {
+              console.log("You have killed the dragon!");
+              slaying = false;
+           }
+           else {
+             youHit = Math.floor(Math.random() * 2);
+          }
+     }
+     else {
+         console.log("The dragon incinerated you!");
+     }
+      slaying = false;
+  }
+}
+else {
+  console.log(Then go away.)
+}
